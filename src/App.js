84b7e9dadb39f6bf08b46ef9bb79acc68dfb116b
bankerneel <neelbanker1@gmail.com>
2022-07-17 16:59:19 +0530

    Updated App.js for Docker logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import docker from './docker.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<a href="https://reactjs.org/">
+					<img src={logo} className="App-logo" alt="logo" />
+				</a>
+				<a href="https://www.docker.com/">
+					<img src={docker} className="App-logo" alt="logo" />
+				</a>
+				&nbsp;&nbsp;&nbsp;&nbsp;
+				<p>
+					Hello React from Docker by &nbsp;
+					<a href="https://github.com/bankerneel/react-sample-docker-app">
+						Neel Banker
+					</a>
+				</p>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
